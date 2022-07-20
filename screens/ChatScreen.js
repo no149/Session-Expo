5a0f4dda86f6b28c122d5db3cf586a6592f1a732
Courses <courses@MacBook-Pro.local>
2022-07-20 15:40:29 +0100

    showingSendButton

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Feather } from '@expo/vector-icons';
@@ -7,7 +7,9 @@ import backgroundImage from '../assets/images/droplet.jpeg';
 import colors from '../constants/colors';
 
 const ChatScreen = props => {
-    
+
+    const [messageText, setMessageText] = useState("");
+
     return (
         <SafeAreaView
             edges={['right', 'left', 'bottom']}
@@ -23,13 +25,28 @@ const ChatScreen = props => {
                     <Feather name="plus" size={24} color={colors.blue} />
                 </TouchableOpacity>
 
-                <TextInput style={styles.textbox} />
+                <TextInput
+                    style={styles.textbox}
+                    value={messageText}
+                    onChangeText={text => setMessageText(text)}/>
 
-                <TouchableOpacity
-                    style={styles.mediaButton}
-                    onPress={() => console.log("Pressed!")}>
-                    <Feather name="camera" size={24} color={colors.blue} />
-                </TouchableOpacity>
+                {
+                    messageText === "" && 
+                    <TouchableOpacity
+                        style={styles.mediaButton}
+                        onPress={() => console.log("Pressed!")}>
+                        <Feather name="camera" size={24} color={colors.blue} />
+                    </TouchableOpacity>
+                }
+
+                {
+                    messageText !== "" && 
+                    <TouchableOpacity
+                        style={styles.mediaButton}
+                        onPress={() => console.log("Pressed!")}>
+                        <Feather name="send" size={24} color={colors.blue} />
+                    </TouchableOpacity>
+                }
             </View>
         </SafeAreaView>
     )
