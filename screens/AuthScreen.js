cf69f7ad655dcbedc51c4f7de38d08cfcca63b51
Courses <courses@MacBook-Pro.local>
2022-07-21 09:11:55 +0100

    customInput

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import Input from '../components/Input';
 import PageContainer from '../components/PageContainer';
 
 const AuthScreen = props => {
     
     return <SafeAreaView style={{ flex: 1}}>
         <PageContainer>
-            <Text>Hello</Text>
+            <Input label="First name" />
         </PageContainer>
     </SafeAreaView>
 };
