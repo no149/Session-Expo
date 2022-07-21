fe47d62cfa614ad044bd4417a36147ca0ee51569
Courses <courses@MacBook-Pro.local>
2022-07-21 16:29:01 +0100

    remainingValidation

--- utils/validationConstraints.js
+++ utils/validationConstraints.js
@@ -17,3 +17,34 @@ export const validateString = (id, value) => {
 
     return validationResult && validationResult[id];
 }
+
+export const validateEmail = (id, value) => {
+    const constraints = { 
+        presence: { allowEmpty: false }
+    };
+
+    if (value !== "") {
+        constraints.email = true
+    }
+
+    const validationResult = validate({ [id]: value }, { [id]: constraints });
+
+    return validationResult && validationResult[id];
+}
+
+export const validatePassword = (id, value) => {
+    const constraints = { 
+        presence: { allowEmpty: false }
+    };
+
+    if (value !== "") {
+        constraints.length = {
+            minimum: 6,
+            message: "must be at least 6 characters"
+        }
+    }
+
+    const validationResult = validate({ [id]: value }, { [id]: constraints });
+
+    return validationResult && validationResult[id];
+}
