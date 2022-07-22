1595b01e1eab0ee9ae7942516c124a256a1e64aa
Courses <courses@MacBook-Pro.local>
2022-07-22 14:43:06 +0100

    showingMainNavigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,10 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import MainNavigator from "./MainNavigator";
 import AuthScreen from "../screens/AuthScreen";
+import { useSelector } from "react-redux";
 
 const AppNavigator = (props) => {
 
-  const isAuth = false;
+  const isAuth = useSelector(state => state.auth.token !== null && state.auth.token !== "");
 
   return (
     <NavigationContainer>
