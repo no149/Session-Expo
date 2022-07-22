3ca1294297fafe37bff7acecc93ab29ac629b0a2
Courses <courses@MacBook-Pro.local>
2022-07-22 17:00:29 +0100

    signingIn

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -1,10 +1,13 @@
-import React, { useCallback, useReducer } from 'react';
+import React, { useCallback, useEffect, useReducer, useState } from 'react';
 import Input from '../components/Input';
 import SubmitButton from '../components/SubmitButton';
 import { Feather } from '@expo/vector-icons';
 
 import { validateInput } from '../utils/actions/formActions';
 import { reducer } from '../utils/reducers/formReducer';
+import { signIn } from '../utils/actions/authActions';
+import { Alert } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 const initialState = {
     inputValues: {
@@ -20,12 +23,38 @@ const initialState = {
 
 const SignInForm = props => {
 
+    const dispatch = useDispatch();
+
+    const [error, setError] = useState();
+    const [isLoading, setIsLoading] = useState(false);
     const [formState, dispatchFormState] = useReducer(reducer, initialState);
 
     const inputChangedHandler = useCallback((inputId, inputValue) => {
         const result = validateInput(inputId, inputValue);
         dispatchFormState({ inputId, validationResult: result, inputValue })
     }, [dispatchFormState]);
+
+    useEffect(() => {
+        if (error) {
+            Alert.alert("An error occured", error, [{ text: "Okay" }]);
+        }
+    }, [error])
+
+    const authHandler = useCallback(async () => {
+        try {
+            setIsLoading(true);
+
+            const action = signIn(
+                formState.inputValues.email,
+                formState.inputValues.password,
+            );
+            dispatch(action);
+            setError(null);
+        } catch (error) {
+            setError(error.message);
+            setIsLoading(false);
+        }
+    }, [dispatch, formState]);
 
     return (
             <>
@@ -51,7 +80,7 @@ const SignInForm = props => {
                 
                 <SubmitButton
                     title="Sign in"
-                    onPress={() => console.log("Button pressed")}
+                    onPress={authHandler}
                     style={{ marginTop: 20 }}
                     disabled={!formState.formIsValid}/>
             </>
