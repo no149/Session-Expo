177bbadc5ff627c65bbfd03c53a0472100959eab
Courses <courses@MacBook-Pro.local>
2022-07-22 09:10:48 +0100

    firebaseHelper

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -6,28 +6,9 @@ import { Feather, FontAwesome } from '@expo/vector-icons';
 import { validateInput } from '../utils/actions/formActions';
 import { reducer } from '../utils/reducers/formReducer';
 import { signUp } from '../utils/actions/authActions';
+import { getFirebaseApp } from '../utils/firebaseHelper';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCf9cdIb7nMkyxUY3Aa36tvkxp_E7WhSfw",
-  authDomain: "whatsapp-d89da.firebaseapp.com",
-  projectId: "whatsapp-d89da",
-  storageBucket: "whatsapp-d89da.appspot.com",
-  messagingSenderId: "173084586629",
-  appId: "1:173084586629:web:bc57e99fffc4b3af506fec",
-  measurementId: "G-EWD53TCBD5"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-console.log(app);
+console.log(getFirebaseApp());
 
 const initialState = {
     inputValues: {
