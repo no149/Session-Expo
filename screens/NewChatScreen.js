cd1503297402d2cdcfe078887870b8ca1f82d41c
Courses <courses@MacBook-Pro.local>
2022-08-20 17:54:42 +0100

    creatingSearchBox

--- screens/NewChatScreen.js
+++ screens/NewChatScreen.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import PageContainer from '../components/PageContainer';
+import { FontAwesome } from '@expo/vector-icons';
+import colors from '../constants/colors';
 
 const NewChatScreen = props => {
 
@@ -18,16 +21,34 @@ const NewChatScreen = props => {
         })
     }, []);
     
-    return <View style={styles.container}>
-        <Text>New chat screen</Text>
-    </View>
+    return <PageContainer>
+        <View style={styles.searchContainer}>
+            <FontAwesome name="search" size={15} color={colors.lightGrey} />
+
+            <TextInput
+                placeholder='Search'
+                style={styles.searchBox}
+                onChangeText={() => {}}
+            />
+        </View>
+    </PageContainer>
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: colors.extraLightGrey,
+        height: 30,
+        marginVertical: 8,
+        paddingHorizontal: 8,
+        paddingVertical: 5,
+        borderRadius: 5
+    },
+    searchBox: {
+        marginLeft: 8,
+        fontSize: 15,
+        width: '100%'
     }
 })
 
