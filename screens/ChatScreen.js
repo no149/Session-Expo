515e7c88b033f578f34f1b4f30107c73bb58b0d4
Courses <courses@MacBook-Pro.local>
2022-09-05 09:03:35 +0100

    replyToComponent

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -20,6 +20,7 @@ import { useSelector } from "react-redux";
 import PageContainer from "../components/PageContainer";
 import Bubble from "../components/Bubble";
 import { createChat, sendTextMessage } from "../utils/actions/chatActions";
+import ReplyTo from "../components/ReplyTo";
 
 const ChatScreen = (props) => {
   const [chatUsers, setChatUsers] = useState([]);
@@ -137,7 +138,10 @@ const ChatScreen = (props) => {
 
           {
             replyingTo &&
-            <Text>Replying to</Text>
+            <ReplyTo
+              text={replyingTo.text}
+              user={storedUsers[replyingTo.sentBy]}
+            />
           }
 
         </ImageBackground>
